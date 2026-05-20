a71383430afc5fe02083877f161c73ab6f9fe0e1
jamesoaress <giftcard1342@gmail.com>
2026-05-19 21:25:47 -0300

    Criando o arquivo dashboard.php

--- src/models/Usuario.php
+++ src/models/Usuario.php
@@ -7,12 +7,12 @@ class Usuario{
     }
 
     public function adicionarTransacao ($usuario_id, $descricao, $valor){
-        $stmt = this->db->prepare("INSERT INTO transacoes (usuario_id, descricao, valor) VALUES (?, ?, ?)");
+        $stmt = $this->db->prepare("INSERT INTO transacoes (usuario_id, descricao, valor) VALUES (?, ?, ?)");
         return $stmt->execute([$usuario_id, $descricao, $valor]);
     }
 
     public function listarTransacoes ($usuario_id){
-        $stmt = this->db->prepare("SELECT descricao, valor FROM transacoes WHERE usuario_id = ?");
+        $stmt = $this->db->prepare("SELECT descricao, valor FROM transacoes WHERE usuario_id = ?");
         $stmt->execute([$usuario_id]);
         return $stmt->fetchAll();
     }
